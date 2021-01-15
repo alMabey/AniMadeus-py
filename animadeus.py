163ae536beb9c618112eeaece1e231157712fbc5
Tomas Ituralde <Tomasiturralde123@gmail.com>
2021-01-15 10:17:34 +0000

    Formatting changed and added comments

--- animadeus.py
+++ animadeus.py
@@ -4,8 +4,10 @@ import mysql.connector
 import subprocess
 
 
+# Class for the bot client.
 class AniMadeus(discord.Client):
 
+    # Constructor for the AniMadeus class.
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
@@ -29,7 +31,7 @@ class AniMadeus(discord.Client):
             'vc': 730475391298568234,
             'graduate': 729027220139409469,
             'minecraft': 758088431065759836,
-            'webmaster' : 335157257346220033,
+            'webmaster': 335157257346220033,
             'member': 472915800081170452,
             'exec': 221311015432749056
         }
@@ -45,10 +47,12 @@ class AniMadeus(discord.Client):
             '⛏️': self.role_ids['minecraft'],
         }
 
-
+    # Method executed when the bot has started up.
     async def on_ready(self):
         await self.change_presence(activity=config.status_activity)
 
+    # Method executed when a reaction is added to a message.
+    # This is used for the role assign system on the server.
     async def on_raw_reaction_add(self, payload):
         if payload.message_id != self.message_ids['role_assign_message']:
             return
@@ -68,6 +72,8 @@ class AniMadeus(discord.Client):
         except discord.HTTPException:
             pass
 
+    # Method executed when a reaction is removed from a message.
+    # This is used for the role assign system on the server.
     async def on_raw_reaction_remove(self, payload):
         if payload.message_id != self.message_ids['role_assign_message']:
             return
@@ -91,52 +97,85 @@ class AniMadeus(discord.Client):
         except discord.HTTPException:
             pass
 
+    # Method executed when a message is sent in the server.
+    # This is used for the various commands.
     async def on_message(self, message):
         if message.channel.id == self.channel_ids['bot-commands'] and message.content[0] == '!':
             message_components = message.content.split()
             if message_components[0] == '!member':
+                # Command to add the "member" role to a user. Requires that they have linked their website account to
+                # their discord.
                 return await self.member_command(message, message_components)
             elif message_components[0] == '!events':
-                return await message.channel.send('{0} - This command is not currently implemented.'.format(message.author.mention))
+                # Command to return upcoming society events using the website's API.
+                return await message.channel.send('{0} - This command is not currently implemented.'.format(
+                    message.author.mention))
             elif message_components[0] == '!library':
-                return await message.channel.send('{0} - This command is not currently implemented.'.format(message.author.mention))
+                # Command to search the society library using the website's API.
+                return await message.channel.send('{0} - This command is not currently implemented.'.format(
+                    message.author.mention))
             else:
                 return await message.channel.send('{0} - This command does not exist.'.format(message.author.mention))
 
         if message.channel.id == self.channel_ids['web-development'] and message.content[0] == '!':
+            # Only members with the Exec role should be able to run these commands. As the command must also be sent in
+            # the #web-development channel, this is limited to The Webmaster, The President and any exec with the Admin
+            # role.
             exec_role = self.get_guild(self.guild_id).get_role(self.role_ids['exec'])
             if exec_role in message.author.roles:
                 message_components = message.content.split()
                 if message_components[0] == '!website_create_users':
+                    # Command to run the createusers command on the website.
                     return await self.website_create_users_command(message, message_components)
                 else:
-                    return await message.channel.send('{0} - This command does not exist.'.format(message.author.mention))
+                    return await message.channel.send('{0} - This command does not exist.'.format(
+                        message.author.mention))
             else:
-                return await message.channel.send('{0} - Only exec can use these commands.'.format(message.author.mention))
+                return await message.channel.send('{0} - Only exec can use these commands.'.format(
+                    message.author.mention))
 
+    # Method for handling the !member command
     async def member_command(self, message, command_components):
         if len(command_components) != 2:
-            return await message.channel.send('{0} - You are using this command incorrectly. The correct usage is `!member <uni_id>`.'.format(message.author.mention))
+            return await message.channel.send(
+                '{0} - You are using this command incorrectly. The correct usage is `!member <uni_id>`.'.format(
+                    message.author.mention))
 
         if len(command_components[1]) != 7:
-            return await message.channel.send('{0} - Your university id should be 7 digits long.'.format(message.author.mention))
+            return await message.channel.send('{0} - Your university id should be 7 digits long.'.format(
+                message.author.mention))
 
         try:
             member_id = int(command_components[1])
         except ValueError:
-            return await message.channel.send('{0} - Your university id should be a 7 digit integer.'.format(message.author.mention))
+            return await message.channel.send('{0} - Your university id should be a 7 digit integer.'.format(
+                message.author.mention))
 
+        # As the bot runs on the same machine as the website, we can connect to the MySQL server that the site DB runs
+        # on to get the discord tag that matches the specified University ID. This is prefereable to using the site API
+        # as Uni ID to discord tag mappings may be considered sensitive by some, so having them on a public API is
+        # a bad idea.
         try:
-            conn = mysql.connector.connect(host=config.db_host, port=config.db_port, database=config.db_name, user=config.db_user, password=config.db_password)
+            conn = mysql.connector.connect(
+                host=config.db_host,
+                port=config.db_port,
+                database=config.db_name,
+                user=config.db_user,
+                password=config.db_password)
         except mysql.connector.Error:
-            await message.channel.send('{0} - An error occurred when running this command, please wait for the webmaster to fix it.'.format(message.author.mention))
+            await message.channel.send(
+                '{0} - An error occurred when running this command, please wait for the webmaster to fix it.'.format(
+                    message.author.mention))
             webmaster = message.guild.get_role(self.role_ids['webmaster'])
             web_development_channel = self.get_guild(self.guild_id).get_channel(self.channel_ids['web-development'])
-            return await web_development_channel.send('{0} - There was an SQL connection error when executing the member command.'.format(webmaster.mention))
+            return await web_development_channel.send(
+                '{0} - There was an SQL connection error when executing the member command.'.format(webmaster.mention))
 
         cursor = conn.cursor()
 
-        query = ('SELECT discord_tag FROM members_member INNER JOIN auth_user ON auth_user.id = members_member.user_id WHERE auth_user.username = %s')
+        query = 'SELECT discord_tag FROM members_member' \
+            'INNER JOIN auth_user ON auth_user.id = members_member.user_id' \
+            'WHERE auth_user.username = %s'
 
         cursor.execute(query, (member_id,))
 
@@ -153,17 +192,25 @@ class AniMadeus(discord.Client):
             await message.author.add_roles(member_role)
             return await message.channel.send('{0} - Member role added.'.format(message.author.mention))
         elif result_found:
-            return await message.channel.send('{0} - The discord tag for this user does not match yours.'.format(message.author.mention))
+            return await message.channel.send('{0} - The discord tag for this user does not match yours.'.format(
+                message.author.mention))
         else:
             return await message.channel.send('{0} - No member with this ID was found.'.format(message.author.mention))
 
+    # Method for handling the !website_create_users command
     async def website_create_users_command(self, message, command_components):
         if len(command_components) != 1:
-            return await message.channel.send('{0} - You are using this command incorrectly. The correct usage is `!website_create_users`.'.format(message.author.mention))
-        
+            return await message.channel.send(
+                '{0} - You are using this command incorrectly. The correct usage is `!website_create_users`.'.format(
+                    message.author.mention))
+
+        # The command needs to be run from the TengenToppaAniMango venv, so we use the subprocess module to execute it.
         process = subprocess.Popen(config.website_create_users_command.split(), stdout=subprocess.PIPE)
+        # The command will either print the number of new members or the error that occurred when trying to make the
+        # accounts.
         output, error = process.communicate()
-        return await message.channel.send('{0} - Command output: `{1}`.'.format(message.author.mention, output.decode('utf-8').strip('\n')))
+        return await message.channel.send('{0} - Command output: `{1}`.'.format(
+            message.author.mention, output.decode('utf-8').strip('\n')))
 
 
 if __name__ == "__main__":
