ece87e0cfe31ec8dd0a2ed71e3aef0298eac2110
Tomas Ituralde <Tomasiturralde123@gmail.com>
2021-01-15 00:20:52 +0000

    Added output to launch

--- launch.sh
+++ launch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Kill running instance of the bot
+echo "Killing old bot instance."
 kill -9 `cat pid.txt`
 rm pid.txt
 
 # Run bot and store pid
 nohup ./venv/bin/python ./animadeus.py &
 echo $! > pid.txt
+echo "Launched new instance."
