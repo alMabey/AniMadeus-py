9b93899eb5a3495ab46226325ebbd153d1c1d756
Tomas Iturralde <Tomasiturralde123@gmail.com>
2022-02-13 23:07:25 +0000

    Valentine for Euan

--- off_topic.py
+++ off_topic.py
@@ -124,18 +124,18 @@ class OffTopicCog(commands.Cog):
             if ':tf:' in message.content.lower() or \
                     ('we do' in message.content.lower() and 'troll' in message.content.lower()):
                 await message.channel.send(random.choices(*zip(*TROLLFACES))[0])
-                
-               
+
+
         elif not message.author.bot:
             # Special February addition
-            if ('no gf' in message.content.lower()):
+            if ('no gf' in message.content.lower() or 'no bf' in message.content.lower()):
                 ctx = await self.bot.get_context(message)
                 await ctx.reply('No! Bad! https://i.imgur.com/PLYtGqI.jpg')
             if ('valentines' in message.content.lower()) or ('valentine' in message.content.lower()):
                 ctx = await self.bot.get_context(message)
                 await ctx.reply(kazuCry)
-                
-                
+
+
     # Bravo Nolan command.
     #
     # Author: Danalite
